7adcda308d60ea753b0aa26d42544d82dd7e183c
Juan Molina <juanmgpt@gmail.com>
2023-08-21 03:29:13 +0200

    Sección 27: CSS3 página de contacto - Proyecto #1

---
+++ 06-maquetación-web1/assets/js/map.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    setTimeout(() => {
+        document.getElementById("load-iframe-map").innerHTML = `
+            <iframe class="contact__iframe"
+                frameborder="0" scrolling="0" marginheight="0" marginwidth="0"
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d194347.4782701837!2d-3.8443433621551946!3d40.43809861028024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422997800a3c81%3A0xc436dec1618c2269!2sMadrid!5e0!3m2!1ses!2ses!4v1692551184401!5m2!1ses!2ses"
+            ></iframe>
+        `;
+    }, 500);
+
+    
+});
